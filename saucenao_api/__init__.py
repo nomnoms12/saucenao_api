ea5a350da9b2bedfacd399dc3f7da80e1a969e34
Alexander Ignatev <alexander.ign0918@gmail.com>
2020-06-16 19:55:20 +0300

    Update License

--- saucenao_api/__init__.py
+++ saucenao_api/__init__.py
@@ -5,7 +5,7 @@ from .containers import BasicSauce, BookSauce, VideoSauce
 __author__ = 'nomnoms12'
 __copyright__ = 'Copyright (c) 2020 nomnoms12'
 __credits__ = ['nomnoms12']
-__license__ = 'MIT'
+__license__ = 'GNU General Public License v3.0'
 __version__ = '1.0.1'
 __maintainer__ = 'nomnoms12'
 __email__ = 'alexander.ign0918@gmail.com'
