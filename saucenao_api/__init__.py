c528c84448c985434c8c4db5f0e71e78cd94e1b6
Alexander Ignatev <alexander.ign0918@gmail.com>
2020-06-16 20:26:43 +0300

    Update License

--- saucenao_api/__init__.py
+++ saucenao_api/__init__.py
@@ -2,11 +2,11 @@ from .saucenao_api import SauceNao
 from .containers import BasicSauce, BookSauce, VideoSauce
 
 
-__author__ = 'nomnoms12'
-__copyright__ = 'Copyright (c) 2020 nomnoms12'
-__credits__ = ['nomnoms12']
+__author__ = 'FujiMakoto'
+__copyright__ = 'Copyright 2020, Taiga.sh Development'
+__credits__ = ['nomnoms12', 'FujiMakoto']
 __license__ = 'GNU General Public License v3.0'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'nomnoms12'
 __email__ = 'alexander.ign0918@gmail.com'
 __status__ = 'Production'
