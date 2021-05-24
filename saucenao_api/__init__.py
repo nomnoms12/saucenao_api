012dd6f86aa7b95c1b1a5706118c346591bdee54
Alexander Ignatev <alexander.ign0918@gmail.com>
2021-05-24 22:52:10 +0300

    Bump version

--- saucenao_api/__init__.py
+++ saucenao_api/__init__.py
@@ -3,9 +3,9 @@ from .saucenao_api import SauceNao
 
 __author__ = 'nomnoms12'
 __copyright__ = 'Copyright 2020, Taiga.sh Development'
-__credits__ = ['nomnoms12', 'FujiMakoto', 'rubinduck', 'spoopcity']
+__credits__ = ['nomnoms12', 'FujiMakoto', 'rubinduck', 'spoopcity', 'bakashigure']
 __license__ = 'GNU General Public License v3.0'
-__version__ = '2.3.1'
+__version__ = '2.3.2'
 __maintainer__ = 'nomnoms12'
 __email__ = 'alexander.ign0918@gmail.com'
 __status__ = 'Production'
